b64ac1c27991dc2dabd33453eeb043c9e4b90602
Sarthak Chopra <sarthak.c96@gmail.com>
2020-06-25 02:50:56 -0500

    Create main.cpp

---
+++ Object-Oriented/Functions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Main
+//
+//  Created by Sarthak Chopra on 6/24/20.
+//  Copyright © 2020 Sarthak Chopra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
